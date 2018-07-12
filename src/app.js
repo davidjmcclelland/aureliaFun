f21e2e3609c8ff7942aacb098f6a889fdea15144
dmcclelland <dmcclelland@ptc.com>
2018-07-11 23:34:54 -0400

    A class method in model is called by button in corresponding view html

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
 export class App {
   constructor() {
     this.message = 'Aurelia Framework!';
+    this.user = 'David J McClelland';
   }
+
+  updateContent() {
+    this.message = 'Aurelia World';
+    this.user = 'David J User';
+  }
+
+  /*
+
+      let firstName = David;
+      let lastName = McClelland;
+
+      this.name = "${firstName} ${lastName}";
+
+      let three = 3;
+      let four = 4;
+
+      this.dozen = "The product of 3 * 4 is ${three * four}."
+
+
+  // decorators
+
+  import ${inject} from 'aurelia-framework';
+  */
+
+// async/await  -- this is used inside a class
+  /*    activate(){
+
+      }*/
 }
