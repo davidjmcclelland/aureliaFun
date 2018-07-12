792a3171e569442cf7f5926ef27c3dcdf6ffa9e2
dmcclelland <dmcclelland@ptc.com>
2018-07-12 12:08:29 -0400

    02-bindingAndComponents

--- src/app.js
+++ src/app.js
@@ -9,26 +9,4 @@ export class App {
     this.user = 'David J User';
   }
 
-  /*
-
-      let firstName = David;
-      let lastName = McClelland;
-
-      this.name = "${firstName} ${lastName}";
-
-      let three = 3;
-      let four = 4;
-
-      this.dozen = "The product of 3 * 4 is ${three * four}."
-
-
-  // decorators
-
-  import ${inject} from 'aurelia-framework';
-  */
-
-// async/await  -- this is used inside a class
-  /*    activate(){
-
-      }*/
 }
