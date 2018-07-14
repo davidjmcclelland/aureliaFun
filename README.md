6e7bf75151c2e0f11af9387cf7c76b47abeccf3e
dmcclelland <dmcclelland@ptc.com>
2018-07-14 07:28:12 -0400

    05-events added Event Aggregator (pub/sub)

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # aureliaFun
-orielly-packt course
 
-https://www.safaribooksonline.com/videos/aurelia-for-beginners/9781789139754/9781789139754-video1_3
+
+[orielly-packt course](https://www.safaribooksonline.com/videos/aurelia-for-beginners/9781789139754/9781789139754-video1_3)
 
 events: Simple events from a button. You don't need the course to figure this one out ; )
+
+[event aggregator](https://www.safaribooksonline.com/videos/aurelia-for-beginners/9781789139754/9781789139754-video2_10) added to this from the following lesson:
 
--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
+
 export class App {
-  constructor() {
+  constructor(eventAggregator) {
+    this.eventAggregator = eventAggregator;
     this.message = 'Aurelia Framework!';
     this.user = 'David J McClelland';
     this.firstName = "Enter your name";
@@ -8,6 +14,22 @@ export class App {
     setInterval(() => this.update(), 1000);
   }
 
+  publish(msg){
+    let payload = msg;
+    this.eventAggregator.publish('myEventName', payload);
+  }
+  subscribe(msg){
+    console.log("subscribe: " + msg);
+    this.subscriber = this.eventAggregator.subscribe('myEventName', payload=> {
+      console.log(payload);
+    });
+  }
+  dispose(msg){
+    console.log("dispose: " + msg);
+    this.subscriber.dispose();
+    // payload is undefined below and you will see an error in console!
+    console.log("payload disposed - payload: " + payload);
+  }
   updateContent() {
     this.message = 'Aurelia World';
     this.user = 'David J User';
